7073854cd9ae1c69d5b788c0f49bf63d62c5072c
Ahmed Ashraf <ahmedabokamel370@gmail.com>
2026-09-11 05:05:18 +0300

    fix turbopack build configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
